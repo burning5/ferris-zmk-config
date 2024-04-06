10561b68392c682ca03b689cb8732f9b42801745
f0e130 <f0e130@me.com>
2024-04-06 18:52:29 +0900

    initial setting

---
+++ config/custom/keys.h
@@ -0,0 +1,138 @@
+// keys
+#define KP_Q__ &kp Q
+#define KP_W__ &kp W
+#define KP_E__ &kp E
+#define KP_R__ &kp R
+#define KP_T__ &kp T
+#define KP_Y__ &kp Y
+#define KP_U__ &kp U
+#define KP_I__ &kp I
+#define KP_O__ &kp O
+#define KP_P__ &kp P
+#define KP_S__ &kp S
+#define KP_D__ &kp D
+#define KP_F__ &kp F
+#define KP_G__ &kp G
+#define KP_H__ &kp H
+#define KP_J__ &kp J
+#define KP_K__ &kp K
+#define KP_L__ &kp L
+#define KP_V__ &kp V
+#define KP_B__ &kp B
+#define KP_N__ &kp N
+#define KP_M__ &kp M
+
+#define KP_1__ &kp N1
+#define KP_2__ &kp N2
+#define KP_3__ &kp N3
+#define KP_4__ &kp N4
+#define KP_5__ &kp N5
+#define KP_6__ &kp N6
+#define KP_7__ &kp N7
+#define KP_8__ &kp N8
+#define KP_9__ &kp N9
+#define KP_0__ &kp N0
+
+#define KP_F1_ &kp F1
+#define KP_F2_ &kp F2
+#define KP_F3_ &kp F3
+#define KP_F4_ &kp F4
+#define KP_F5_ &kp F5
+#define KP_F6_ &kp F6
+#define KP_F7_ &kp F7
+#define KP_F8_ &kp F8
+#define KP_F9_ &kp F9
+#define KP_F10 &kp F10
+#define KP_F11 &kp F11
+#define KP_F12 &kp F12
+
+#define KP_ESC &kp ESCAPE
+#define KP_TAB &kp TAB
+#define KP_SPC &kp SPACE
+#define KP_BSP &kp BACKSPACE
+#define KP_EXC &kp EXCL
+#define KP_AT_ &kp AT
+#define KP_HSH &kp HASH
+#define KP_DLR &kp DLLR
+#define KP_PCT &kp PRCNT
+#define KP_CRT &kp CARET
+#define KP_AMP &kp AMPS
+#define KP_AST &kp ASTRK
+#define KP_UND &kp UNDER
+#define KP_EQL &kp EQUAL
+#define KP_GRV &kp GRAVE
+#define KP_TID &kp TILDE
+#define KP_LBK &kp LBKT
+#define KP_RBK &kp RBKT
+#define KP_BSL &kp BSLH
+#define KP_APS &kp APOS
+#define KP_MIN &kp MINUS
+#define KP_DQT &kp DQT
+#define KP_PIP &kp PIPE
+#define KP_SLS &kp KP_SLASH
+
+#define KP_PSC &kp PSCRN
+#define KP_SLK &kp SLCK
+#define KP_NLK &kp LNLCK
+#define KP_BRK &kp PAUSE_BREAK
+
+#define KP_INS &kp INSERT
+#define KP_DEL &kp DELETE
+#define KP_HOM &kp HOME
+#define KP_END &kp END
+
+#define KP_CTL &kp LCTRL
+#define KP_GUI &kp LGUI
+#define KP_ALT &kp LALT
+#define KP_SFT &kp LSHIFT
+
+#define KP_PUP &kp PAGE_UP
+#define KP_PDN &kp PAGE_DOWN
+#define KP_KMI &kp KP_MINUS
+#define KP_PLS &kp KP_PLUS
+#define KP_DT_ &kp KP_DOT
+
+#define KP_AU_ &kp UP
+#define KP_AR_ &kp RIGHT
+#define KP_AL_ &kp LEFT
+#define KP_AD_ &kp DOWN
+
+
+// capsword
+#define CP_WRD &caps_word
+
+// one shot keys
+#define OS_SCP &kp LG(LS(S))
+
+#define MT_A__ &mt LSHIFT A
+#define MT_SMC &mt LSHIFT SEMICOLON
+#define MT_Z__ &mt LCTRL Z
+#define MT_X__ &mt LGUI X
+#define MT_C__ &mt LALT C
+#define MT_CMA &mt LALT COMMA
+#define MT_DOT &mt LGUI DOT
+#define MT_SLS &mt LCTL SLASH
+
+#define TD_COP &TD_CCP
+#define TD_LPR &TD_LBKT
+#define TD_RPR &TD_RBKT
+#define TD_LAN &TD_LANG
+
+// layers
+#define MO_SYM &mo 1
+#define MO_NUM &mo 2
+#define MO_CFG &mo 4
+
+#define LT_ENT &lt 3 ENTER
+
+// bluetooth
+#define BT_CLA &bt BT_CLR
+#define BT_SE0 &bt BT_SEL 0
+#define BT_SE1 &bt BT_SEL 1
+#define BT_SE2 &bt BT_SEL 2
+#define BT_SE3 &bt BT_SEL 3
+#define BT_SE4 &bt BT_SEL 4
+
+// quantum
+#define __XX__ &none
+#define _TRNS_ &trans
